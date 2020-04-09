807eaebbf7c43d1e8ce47497f4560b37509f548b
0x131cc05 <0x131cc05@gmail.com>
2020-04-09 20:18:20 +0800

    update

--- Other/tmp.cpp
+++ Other/tmp.cpp
@@ -1,46 +1,112 @@
-#include <bits/stdc++.h>
+#include "iostream"
+#include "algorithm"
+#include "cstring"
+#include "cstdio"
+#include "cmath"
+#include "vector"
+#include "map"
+#include "set"
+#include "queue"
 using namespace std;
-typedef long long LL;
-const int N = 250010, mod = 998244353;
-inline int Pow(int x, int y) {
-    int res = 1;
-    for (;y;y >>= 1, x = (LL)x * x % mod) if (y & 1) res = (LL)res * x % mod;
-    return res;
+#define MAXN 100006
+//#define int long long
+#define rep(i, a, b) for (int i = (a), i##end = (b); i <= i##end; ++i)
+#define per(i, a, b) for (int i = (a), i##end = (b); i >= i##end; --i)
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define vi vector<int>
+#define all(x) (x).begin() , (x).end()
+#define mem( a ) memset( a , 0 , sizeof a )
+#define P 998244353
+typedef long long ll;
+int n , m , k;
+int A[MAXN] , ch[MAXN];
+vector<pii> G[MAXN] , pr;
+int p2[MAXN];
+
+int siz[MAXN] , ea;
+int g[MAXN][19] , dep[MAXN];
+ll dis[MAXN];
+
+int J[MAXN] , iJ[MAXN];
+int C( int a , int b ) {
+    if( a < b ) return 0;
+    return J[a] * 1ll * iJ[b] % P * iJ[a - b] % P;
 }
-int r[N * 4];
-inline void NTT(int a[], int len, int type) {
-    for (int i = 1;i < len;i++) if (i < r[i]) swap(a[i], a[r[i]]);
-    for (int mid = 2;mid <= len;mid <<= 1) {
-        int Wn = Pow(3, type ? (mod - 1) / mid : mod - 1 - (mod - 1) / mid);
-        for (int i = 0;i < len;i += mid)
-            for (int j = i, t, w = 1;j < i + (mid >> 1);j++, w = (LL)w * Wn % mod)
-                t = (LL)w * a[j + (mid >> 1)] % mod, a[j + (mid >> 1)] = (a[j] - t + mod) % mod, a[j] = (a[j] + t) % mod;
+
+void dfs( int u , int fa ) {
+    siz[u] = ch[u];
+    for( auto& t : G[u] ) if( t.fi != fa ) {
+            int v = t.fi;
+            g[v][0] = u , dep[v] = dep[u] + 1 , dis[v] = dis[u] + t.se;
+            for( int k = 1 ; k < 19 ; ++ k )
+                if( g[g[v][k - 1]][k - 1] ) g[v][k] = g[g[v][k - 1]][k - 1];
+                else break;
+            dfs( v , u );
+            siz[u] += siz[v];
+            ( ea += 1ll * t.se * ( ( C( m , k ) - C( siz[v] , k ) + P ) % P - C( m - siz[v] , k ) + P ) % P ) %= P;
+        }
+}
+int lca( int u , int v ) {
+    if( dep[u] < dep[v] ) swap( u , v );
+    for( int k = 18 ; k >= 0 ; -- k ) if( dep[g[u][k]] >= dep[v] ) u = g[u][k];
+    if( u == v ) return u;
+    for( int k = 18 ; k >= 0 ; -- k )
+        if( g[u][k] != g[v][k] ) u = g[u][k] , v = g[v][k];
+    return g[u][0];
+}
+
+ll d[506][506];
+
+int Pow( int x , int a ) {
+    int ret = 1;
+    while( a ) {
+        if( a & 1 ) ret = 1ll * ret * x % P;
+        x = 1ll * x * x % P , a >>= 1;
     }
-    if (!type) for (int i = 0, inv = Pow(len, mod - 2);i < len;i++)
-        a[i] = (LL)a[i] * inv % mod;
-}  
-int frac[N], inv[N], A[N * 4], B[N * 4];
-int main() { 
-    freopen("in1.in", "r", stdin), freopen("in1.out", "w", stdout); 
-    int n; LL m; scanf("%d%lld", &n, &m), frac[0] = 1, m %= mod - 1;
-    for (int i = 1;i <= n;i++) frac[i] = (LL)frac[i - 1] * i % mod;
-    inv[n] = Pow(frac[n], mod - 2);
-    for (int i = n - 1;i >= 0;i--) inv[i] = (LL)inv[i + 1] * (i + 1) % mod;
-    for (int i = 0;i <= n;i++) scanf("%d", &A[i]), A[i] = (LL)A[i] * frac[i] % mod;
-    for (int i = 0;i <= n;i++) B[n - i] = inv[i];
-    int len = 1, l = 0;
-    while (len <= n * 2) len <<= 1, l++;
-    for (int i = 1;i < len;i++) r[i] = (r[i >> 1] >> 1) | ((i & 1) << l - 1);
-    NTT(A, len, 1), NTT(B, len, 1);
-    for (int i = 0;i < len;i++) A[i] = (LL)A[i] * B[i] % mod;
-    NTT(A, len, 0);
-    for (int i = 0;i <= n;i++) A[i] = A[i + n];
-    for (int i = n + 1;i < len;i++) A[i] = 0;
-    for (int i = 0;i <= n;i++) A[i] = (LL)A[i] % mod * Pow(Pow(i + 1, m), mod - 2) % mod;
-    for (int i = n + 1;i < len;i++) B[i] = 0;
-    for (int i = 0;i <= n;i++) B[n - i] = i & 1 ? mod - inv[i] : inv[i];
-    NTT(A, len, 1), NTT(B, len, 1);
-    for (int i = 0;i < len;i++) A[i] = (LL)A[i] * B[i] % mod;
-    NTT(A, len, 0);
-    for (int i = 0;i <= n;i++) printf("%d ", (LL)A[i + n] * inv[i] % mod);
+    return ret;
 }
+
+void solve() {
+//    cout << 10ll * Pow( 3 , P - 2 ) % P << endl;
+    cin >> n >> m >> k;
+    p2[0] = J[0] = iJ[0] = 1;
+    rep( i , 1 , n ) p2[i] = p2[i - 1] * 2 % P , J[i] = 1ll * J[i - 1] * i % P , iJ[i] = Pow( J[i] , P - 2 );
+    int u , v , w;
+    rep( i , 1 , m ) scanf("%d",A + i) , ch[A[i]] = 1;
+    rep( i , 2 , n ) {
+        scanf("%d%d%d",&u,&v,&w) , G[u].eb( mp( v , w ) ) , G[v].eb( mp( u , w ) );
+    }
+    dep[1] = 1 , dfs( 1 , 1 );
+    ea = ea * 2 % P;
+    rep( i , 1 , m ) rep( j , i , m ) d[i][j] = d[j][i] = dis[A[i]] + dis[A[j]] - 2 * dis[lca( A[i] , A[j] )];
+    int ok , res = 0;
+    ll rd , re;
+//    cout << lca( A[3] , A[4] ) << endl;
+    rep( i , 1 , m ) {
+        rep( j , 1 , i - 1 ) {
+            rd = d[i][j] , ok = 2;
+            rep(t, 1, m) if (t != i && t != j) {
+                if (max(d[t][i], d[t][j]) > rd) continue;
+                if (d[t][i] == rd && t < j) continue;
+                if (d[t][j] == rd && t < i) continue;
+                ok++;
+            }
+            ( res += 1ll * C( ok - 2 , k - 2 ) * ( rd % P ) % P ) %= P;
+        }
+    }
+//    cout << res << ' ' << ea << endl;
+    cout << 1ll * ( ea - res + P ) % P * ( Pow( C( m , k ) , P - 2 ) ) % P << endl;
+}
+
+signed main() {
+//    freopen("in1.in","r",stdin);
+//    freopen("fuckout","w",stdout);
+//    int T;cin >> T;while( T-- ) solve();
+    solve();
+}
+
